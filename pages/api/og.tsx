7ce37ffcb0e421cd26eb39666a984ebefc3d070c
injoon <injoon5@naver.com>
2023-01-05 23:12:00 +0900

    Edge OK..?

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 
 export const config = {
-  runtime: '',
+  runtime: 'experimental-edge',
 }
 
 export default function handler(req: NextRequest) {
